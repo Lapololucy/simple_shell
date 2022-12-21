889043fe885ec9785025bbee3293bd247871cbb0
Lapololucy <lapololucy@gmail.com>
2022-12-21 00:56:41 -0800

    simple shell project

--- shell.h
+++ shell.h
@@ -38,14 +38,13 @@ extern char **environ;
 
 
 /**
- *  * struct liststr - singly linked list
- *   * @num: the number field
- *    * @str: a string
- *     * @next: points to the next node
- *      */
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
 typedef struct liststr
 {
-<<<<<<< HEAD
 	int num;
 	char *str;
 	struct liststr *next;
@@ -94,61 +93,10 @@ typedef struct passinfo
 	int cmd_buf_type; /* CMD_type ||, &&, ; */
 	int readfd;
 	int histcount;
-=======
-		int num;
-			char *str;
-				struct liststr *next;
-} list_t;
-
-/**
- *  *struct passinfo - contains pseudo-arguements to pass into a function,
- *   *		allowing uniform prototype for function pointer struct
- *    *@arg: a string generated from getline containing arguements
- *     *@argv: an array of strings generated from arg
- *      *@path: a string path for the current command
- *       *@argc: the argument count
- *        *@line_count: the error count
- *         *@err_num: the error code for exit()s
- *          *@linecount_flag: if on count this line of input
- *           *@fname: the program filename
- *            *@env: linked list local copy of environ
- *             *@environ: custom modified copy of environ from LL env
- *              *@history: the history node
- *               *@alias: the alias node
- *                *@env_changed: on if environ was changed
- *                 *@status: the return status of the last exec'd command
- *                  *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *                   *@cmd_buf_type: CMD_type ||, &&, ;
- *                    *@readfd: the fd from which to read line input
- *                     *@histcount: the history line number count
- *                      */
-typedef struct passinfo
-{
-		char *arg;
-			char **argv;
-				char *path;
-					int argc;
-						unsigned int line_count;
-							int err_num;
-								int linecount_flag;
-									char *fname;
-										list_t *env;
-											list_t *history;
-												list_t *alias;
-													char **environ;
-														int env_changed;
-															int status;
-
-																char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-																	int cmd_buf_type; /* CMD_type ||, &&, ; */
-																		int readfd;
-																			int histcount;
->>>>>>> 0615b1e4a79d63a93041fe0e704a360db8fdbefc
 } info_t;
 
 #define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-<<<<<<< HEAD
 	0, 0, 0}
 
 /**
@@ -160,19 +108,6 @@ typedef struct builtin
 {
 	char *type;
 	int (*func)(info_t *);
-=======
-		0, 0, 0}
-
-/**
- *  *struct builtin - contains a builtin string and related function
- *   *@type: the builtin command flag
- *    *@func: the function
- *     */
-typedef struct builtin
-{
-		char *type;
-			int (*func)(info_t *);
->>>>>>> 0615b1e4a79d63a93041fe0e704a360db8fdbefc
 } builtin_table;
 
 
